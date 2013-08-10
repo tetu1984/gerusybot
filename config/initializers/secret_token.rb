30695c6ce79296c3b1245c8f3066963d2befbe80
tetu1984 <tetu1984@gmail.com>
2013-08-10 13:03:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gerusybot::Application.config.secret_token = 'a688dd666517162428c3fc8a0cc8fe7b6fba6a782f1f47a3c283399d4df44de775bb69d3eb7112cf6b54dfd88ce9285c9feb7ddf39f1acd4816521909157bb25'
